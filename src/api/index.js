802bd7b422be3cbd9d014aa6631b7623be100646
YozhEzhi <yozhezhi@gmail.com>
2017-04-29 02:15:57 +0300

    Deploy resetup with surge

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
 export const API_KEY = 'e539b3dcdce62f43d0c9eac4ff2b6ab4';
-export const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
