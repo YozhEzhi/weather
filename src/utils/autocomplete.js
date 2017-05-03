10d99e52b1259e2af59d57c090a5ca8f67ab546f
YozhEzhi <yozhezhi@gmail.com>
2017-05-04 00:17:58 +0300

    formatted_address fix

--- src/utils/autocomplete.js
+++ src/utils/autocomplete.js
@@ -1,15 +1,12 @@
 const google = window.google;
 
 export default function initAutocomplete(elementId, updatePlaces, dropState) {
-  const options = {
-    types: ['(cities)'],
-  };
   const searchField = document.getElementById(elementId);
-  const places = new google.maps.places.Autocomplete(searchField, options);
+  const places = new google.maps.places.Autocomplete(searchField);
 
   google.maps.event.addListener(places, 'place_changed', () => {
-    const placeName = places.gm_accessors_.place.Fc.formattedPrediction;
     const place = places.getPlace();
+    const placeName = place.formatted_address;
     const lat = place.geometry.location.lat();
     const lon = place.geometry.location.lng();
 
